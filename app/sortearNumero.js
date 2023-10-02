30b283bcc3d74d4771b304c906634c691ce7f00a
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2023-10-02 11:51:40 -0300

    aula dia 02/10

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor=1
+const maiorValor=1000
+
+const numeroSecreto = gerarNumeroAleatoril()
+
+function  gerarNumeroAleatoril(){
+   return parseInt(Math.random()*maiorValor + 1)
+}
+
+console.log('Numero Secreto:',numeroSecreto)
+
+const elementoMemorValor = document.getElementById('menor-valor')
+elementoMemorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
